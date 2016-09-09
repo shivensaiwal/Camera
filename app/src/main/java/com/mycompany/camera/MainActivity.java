39ac71e1713068a06f1f185fc75d5ac0d2fd9a7b
shivensaiwal <saiwal.shiven@gmail.com>
2016-09-08 23:23:20 -0700

    Error Updated

--- app/src/main/java/com/mycompany/camera/MainActivity.java
+++ app/src/main/java/com/mycompany/camera/MainActivity.java
@@ -13,10 +13,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.Settings;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,17 +32,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         btnTakePhoto = (Button) findViewById(R.id.button1);
         imgTakenPhoto = (ImageView) findViewById(R.id.imageview1);
         btnTakePhoto.setOnClickListener(new btnTakePhotoClicker());
